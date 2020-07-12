600c598a9a747098e72f3d5516ad5656f28cd82f
Valentin Vila <vvila@Valentins-MacBook-Pro.local>
2020-07-12 02:13:49 -0300

    without ID, clean code, check for update or new package

--- flask_packages/scripts/scrapper.py
+++ flask_packages/scripts/scrapper.py
@@ -10,9 +10,9 @@ def db_connection():
     mongopasswd = os.environ["MONGOPASSWD"]
 
     cluster = MongoClient("mongodb+srv://user:"+mongopasswd+"@packagescluster.3irbh.mongodb.net/test?retryWrites=true&w=majority")
-    db = cluster['flaskdb']
-    collection = db['flaskcollection']
-    collection.create_index([("name", pymongo.TEXT)], unique=True)
+    db = cluster['flask_packages_db']
+    collection = db['flask_packages_collection']
+    #collection.create_index([("name", pymongo.TEXT)], unique=True) LO CREE EN COMPASS
 
     search_packages(collection)
 
@@ -37,15 +37,22 @@ def search_packages(collection):
             ### NAME ###
             name = package_info.find('span',{'class':'package-snippet__name'}).text
             # Search the name of the package in the db, if exists, check information for updates. If not, add them to the db.
-            find_on_db = collection.find({"name":name}).count()
-            if find_on_db:
+            find_on_db = list(collection.find({"name":name})) ### VOLVI A LA LISTA PQ NO LO PUDE HACER FUNCIONAR CON EL COL.FIND().COUNT()
+            if find_on_db == []:
                 add_package_to_db(package_info, name, collection)
             else:
                 update_package_on_db(package_info, name, collection)
 
 def add_package_to_db(package_info, name, collection):
     
-    version = package_info.find('span',{'class':'package-snippet__version'}).text
+    ENDC = '\033[m'
+    TYELLOW = '\033[93m'
+    print(TYELLOW + "agregando: ",name, ENDC)
+
+    try:
+        version = package_info.find('span',{'class':'package-snippet__version'}).text
+    except:
+        version = ""
     
     released = package_info.find('span',{'class':'package-snippet__released'}).text.strip()
 
@@ -81,21 +88,28 @@ def add_package_to_db(package_info, name, collection):
         "released":released,
         "homepage":homepage_link
     }
-
-    collection.insert(package)
+    collection.insert_one(package)
 
 def update_package_on_db(package_info, name, collection):
-    #print('actulizando',name)
-    
-    old_package = collection.find_one({"name":name}) 
-    
+    ENDC = '\033[m'
+    TGREEN = '\033[32m'
+    print(TGREEN + "actulizando",name, ENDC)
+
+    ### NO PODEMOS HACER UN APPEND A UN CURSOR DE MONGO
+
     ### VERSION ###
-    version = package_info.find('span',{'class':'package-snippet__version'}).text
     
-    if version in old_package["version"]:
+    #try:
+    version = package_info.find('span',{'class':'package-snippet__version'}).text
+    search = collection.find({"name":name})
+    for item in search:
+        version_db = item["version"]
+    if version == version_db[0]:
         return
+    version = ""
+    print('siguio')
+    old_package = []
     old_package["version"].append(version)
-        
     ### RELEASED ###
     released = package_info.find('span',{'class':'package-snippet__released'}).text.strip()
     old_package["released"] = released
@@ -136,11 +150,11 @@ def update_package_on_db(package_info, name, collection):
     maintainer = maintainer_find.text.strip()
 
     old_package["maintainer"] = maintainer
-    
-    collection.update_one({'name': old_package['name']}, {'$set': old_package})
+    collection.update_one({'name': name}, {'$set': old_package})
 
 if __name__ == "__main__":
     db_connection()
 
 
-    ### DEVOLVER ESTO
+    ### DEVOLVER ESTO 
+    ### https://pypi.org/project/cinq-auditor-vpc-flowlogs/2.1.1/#files OBTENER SHA256 y LINK`
