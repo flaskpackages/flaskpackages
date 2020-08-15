5220424f6d61948e838cd5667ec5711c71cf62b1
Leonardo Lazzaro <leonardol@infobytesec.com>
2020-08-15 19:52:18 -0300

    add initial model

--- flask_packages/models.py
+++ flask_packages/models.py
@@ -20,6 +20,43 @@ class User(db.Document, UserMixin):
     confirmed_at = db.DateTimeField()
     roles = db.ListField(db.ReferenceField(Role), default=[])
 
+
+class Version(db.Document):
+    version = db.StringField()
+    date = db.DateTimeField()
+    link = db.StringField()
+    sha256 = db.StringField()
+
+
+class Classifier(db.Document):
+    development_status = db.ListField(db.StringField())
+    programming_language = db.ListField(db.StringField())
+    topic = db.ListField(db.StringField())
+
+
+class GithubStats(db.Document):
+    stars = db.IntField()
+    open_pr = db.IntField()
+    forks = db.IntField()
+
+
+class Project(db.Document):
+    name = db.StringField(unique=True)
+    description = db.StringField()
+    lastest_version = db.StringField()
+    maintainer = db.StringField()
+    homepage = db.StringField()
+    pypi_link = db.StringField(unique=True)
+
+    versions = db.ListField(db.EmbeddedDocumentField(Version))
+    github_stats = db.EmbeddedDocumentField(GithubStats)
+    tags = db.ListField(db.StringField())
+    classifiers = db.ListField(db.EmbeddedDocumentField(Classifier))
+
+    release = db.DateTimeField()
+    #category = db.ListField(db.Strin
+
+
 # Setup Flask-Security
 user_datastore = MongoEngineUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
